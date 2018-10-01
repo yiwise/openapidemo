d390a9f06b5f68fd892edb20ae3eb2b984da0b2c
yangdehong@yiwise.com <ydh199078>
2018-10-01 19:03:00 +0800

    任务信息

--- src/main/java/com/yiwise/SimpleDemo.java
+++ src/main/java/com/yiwise/SimpleDemo.java
@@ -22,11 +22,21 @@ public class SimpleDemo {
     public static final String TENANT_SIGN = "yiwise";
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String result = getCompanyList();
+//        String result = getCompanyList();
 //        String result = getPhoneList();
+//        String result = getRobotList();
+//        String result = getTasks();
+//        String result = getTaskById();
+//        String result = getCallRecordInfoList();
+        String result = callDetail();
         System.out.println(result);
     }
 
+    /**
+     * 公司列表
+     * @return
+     * @throws UnsupportedEncodingException
+     */
     private static String getCompanyList() throws UnsupportedEncodingException {
         String url = "http://localhost:8060/openApi/v1/company/getCompanies?";
         TreeMap<String,  Object> req = new TreeMap<>();
@@ -41,9 +51,119 @@ public class SimpleDemo {
         String result = HttpUrlConnectionUtils.doGet(url, param.toString(), APP_KEY, APP_SECRET, TENANT_SIGN);
         return result;
     }
+
+    /**
+     * 电话线列表
+     * @return
+     * @throws UnsupportedEncodingException
+     */
     private static String getPhoneList() throws UnsupportedEncodingException {
         String url = "http://localhost:8060/openApi/v1/company/getPhones?";
         TreeMap<String,  Object> req = new TreeMap<>();
+        req.put("timestamp", System.currentTimeMillis());
+        req.put("signature", SignUtils.sign(req, TENANT_SIGN));
+
+        StringBuilder param = new StringBuilder();
+        for (Iterator<Map.Entry<String, Object>> it = req.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, Object> e = it.next();
+            param.append(e.getKey()).append("=").append(URLEncoder.encode(e.getValue().toString(), "UTF-8")).append("&");
+        }
+        String result = HttpUrlConnectionUtils.doGet(url, param.toString(), APP_KEY, APP_SECRET, TENANT_SIGN);
+        return result;
+    }
+
+    /**
+     * 机器人列表
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private static String getRobotList() throws UnsupportedEncodingException {
+        String url = "http://localhost:8060/openApi/v1/company/getRobots?";
+        TreeMap<String,  Object> req = new TreeMap<>();
+        req.put("timestamp", System.currentTimeMillis());
+        req.put("signature", SignUtils.sign(req, TENANT_SIGN));
+
+        StringBuilder param = new StringBuilder();
+        for (Iterator<Map.Entry<String, Object>> it = req.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, Object> e = it.next();
+            param.append(e.getKey()).append("=").append(URLEncoder.encode(e.getValue().toString(), "UTF-8")).append("&");
+        }
+        String result = HttpUrlConnectionUtils.doGet(url, param.toString(), APP_KEY, APP_SECRET, TENANT_SIGN);
+        return result;
+    }
+
+    /**
+     * 获取任务列表
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private static String getTasks() throws UnsupportedEncodingException {
+        String url = "http://localhost:8060/openApi/v1/task/getTasks?";
+        TreeMap<String,  Object> req = new TreeMap<>();
+        req.put("name", "dialog");
+        req.put("status", "TERMINATE");
+        req.put("pageNum", 1);
+        req.put("pageSize", 20);
+        req.put("timestamp", System.currentTimeMillis());
+        req.put("signature", SignUtils.sign(req, TENANT_SIGN));
+
+        StringBuilder param = new StringBuilder();
+        for (Iterator<Map.Entry<String, Object>> it = req.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, Object> e = it.next();
+            param.append(e.getKey()).append("=").append(URLEncoder.encode(e.getValue().toString(), "UTF-8")).append("&");
+        }
+        String result = HttpUrlConnectionUtils.doGet(url, param.toString(), APP_KEY, APP_SECRET, TENANT_SIGN);
+        return result;
+    }
+    /**
+     * 获取某个任务信息
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private static String getTaskById() throws UnsupportedEncodingException {
+        String url = "http://localhost:8060/openApi/v1/task/getTaskDetail?";
+        TreeMap<String,  Object> req = new TreeMap<>();
+        req.put("robotCallJobId", 3);
+        req.put("timestamp", System.currentTimeMillis());
+        req.put("signature", SignUtils.sign(req, TENANT_SIGN));
+
+        StringBuilder param = new StringBuilder();
+        for (Iterator<Map.Entry<String, Object>> it = req.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, Object> e = it.next();
+            param.append(e.getKey()).append("=").append(URLEncoder.encode(e.getValue().toString(), "UTF-8")).append("&");
+        }
+        String result = HttpUrlConnectionUtils.doGet(url, param.toString(), APP_KEY, APP_SECRET, TENANT_SIGN);
+        return result;
+    }
+    /**
+     * 获取通话记录
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private static String getCallRecordInfoList() throws UnsupportedEncodingException {
+        String url = "http://localhost:8060/openApi/v1/task/getCallRecordInfoList?";
+        TreeMap<String,  Object> req = new TreeMap<>();
+        req.put("robotCallJobId", 3);
+        req.put("timestamp", System.currentTimeMillis());
+        req.put("signature", SignUtils.sign(req, TENANT_SIGN));
+
+        StringBuilder param = new StringBuilder();
+        for (Iterator<Map.Entry<String, Object>> it = req.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, Object> e = it.next();
+            param.append(e.getKey()).append("=").append(URLEncoder.encode(e.getValue().toString(), "UTF-8")).append("&");
+        }
+        String result = HttpUrlConnectionUtils.doGet(url, param.toString(), APP_KEY, APP_SECRET, TENANT_SIGN);
+        return result;
+    }
+    /**
+     * 获取通话记录
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private static String callDetail() throws UnsupportedEncodingException {
+        String url = "http://localhost:8060/openApi/v1/task/callDetail?";
+        TreeMap<String,  Object> req = new TreeMap<>();
+        req.put("callRecordId", 1);
         req.put("timestamp", System.currentTimeMillis());
         req.put("signature", SignUtils.sign(req, TENANT_SIGN));
 
